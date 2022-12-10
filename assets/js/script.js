2a02c04eb54b6588a7a34240309dbd854e84c604
ChrisLPlumb91 <chrislplumb91@hotmail.com>
2022-12-10 21:45:02 +0000

    Add missing code

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,6 +76,12 @@ function displayErrors(data) {
         results = `<div class="no_errors">No errors reported!</div>`;
     } else {
         results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`;
+
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.col}:</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
     }
 
     document.getElementById("resultsModalTitle").innerText = heading;
